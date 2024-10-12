efec5568f48181faa13f9e25fe47f05376086dc3
massitaverna <massi.taverna@gmail.com>
2024-10-12 15:38:15 +0200

    capital B in TestPlugBoard class name

--- enigma/tests/test_components.py
+++ enigma/tests/test_components.py
@@ -4,7 +4,7 @@ import unittest
 from lib.internals import PlugBoard
 
 
-class TestPlugboard(unittest.TestCase):
+class TestPlugBoard(unittest.TestCase):
     def test_identity_mapping(self):
         p = PlugBoard(n_wires=0)
         for c in string.ascii_lowercase:
